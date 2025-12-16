4e226512dab44d86a32e0bc236a4c33a9c629dec
Dave Keiser <davefkeiser@gmail.com>
2025-12-15 22:53:12 -0600

    start cart edit consistency work

--- public/main.js
+++ public/main.js
@@ -7,7 +7,7 @@
  * - Quick View modal for options like size / name / special request
  * - Cart stores per-item options (size, dog name, note, color)
  */
-// cart edit consistency branch
+// cart edit consistency branch   and again
 
 // ------------------------------
 // STATE
